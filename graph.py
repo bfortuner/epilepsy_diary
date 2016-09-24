f344edad8fe3ca89fdad99afa540e3c0c196af32
Fortuner <fortuner@f45c89a3da2d.ant.amazon.com>
2016-09-23 17:56:36 -0700

    Updated code to use real data

--- graph.py
+++ graph.py
@@ -8,41 +8,40 @@ from diary_app.clients import s3
 CHARTS_DIR_PATH="graphs/"
 CHARTS_S3_BUCKET="hapibot-graph"
 
-
-def createChart(data):
-	filename = id_generator.generate_chart_image_filename()
+def create_chart(input_data):
 	py.sign_in('hapibot', 'ix1yikrn67') # Replace the username, and API key with your credentials.
-
-	x = []
-	Seizures = go.Scatter(
-	    x=[1, 2, 3, 4],
-	    y=[0, 2, 3, 5],
+	seizures = go.Scatter(
+	    x=input_data["x"]["data"],
+	    y=input_data["y"]["data"],
 	    fill='tozeroy'
 	)
+	chart_data=[seizures]
+	chart_layout = dict(title = 'Seizures (Last 7 Days)',
+              xaxis = dict(title = input_data["x"]["label"]),
+              yaxis = dict(title = input_data["y"]["label"]),
+              width=800, 
+              height=640)
+	chart = go.Figure(data=chart_data, layout=chart_layout)
+	filename = id_generator.generate_chart_image_filename()
+	py.image.save_as(chart, filename=CHARTS_DIR_PATH+filename)
 
-	data=[Seizures]
-	layout = go.Layout(title='Seizures', width=800, height=640)
-	fig = go.Figure(data=data, layout=layout)
-
-	py.image.save_as(fig, filename=CHARTS_DIR_PATH+filename)
-	# conn = s3.get_connection()
 	s3.upload_file(filename, CHARTS_DIR_PATH+filename, CHARTS_S3_BUCKET)
-
 	download_url = s3.get_download_url(bucket=CHARTS_S3_BUCKET, path=filename, expiry=603148) #7 days
+	
 	return download_url
 
-data = [
-    {
-      'date': '02-10-2016',
-      'count': 3
+data = {
+    "x" : {
+      'label': "Date",
+      'data': ["Sep 22", "Sep 23", "Sep 24", "Sep 25", "Sep 26", "Sep 27", "Sep 28"]
     },
-    {
-      'date': '02-11-2016',
-      'count': 5
+    "y" : {
+      'label': "Count",
+      'data': [1,3,2,6,3,2,7]
     }
-]
+}
 
 if __name__ == "__main__":
-	print createChart(data)
+	print create_chart(data)
 
 
