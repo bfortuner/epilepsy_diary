0be4754de9bf2fa9291b36638825abc7900dc597
bfortuner <bfortuner@gmail.com>
2016-09-26 08:49:27 -0700

    Added EventTrackStatus model

--- create_db.py
+++ create_db.py
@@ -1,14 +1,15 @@
 from diary_app.database import db, init_db
-from diary_app.events.models import Event
+from diary_app.events.models import Event, EventTrackingStatus
 from diary_app.users.models import User
 from diary_app.users.constants import ADMIN_USERNAME
+from diary_app.events.constants import EVENT_TRACKING_STATUS_TYPES
 
 
 """
 DO NOT RUN THIS SCRIPT IN ''PROD'' IF DATABASE ALREADY HAS LIVE DATA!
 """
 
-# Drop and recreate DB
+# # Drop and recreate DB
 init_db()
 
 print "DB tables created"
@@ -20,8 +21,16 @@ db.commit()
 
 print "User table loaded"
 
+# # Insert Event Tracking Status Type
+for status in EVENT_TRACKING_STATUS_TYPES:
+	event_tracking_status = EventTrackingStatus(status)
+	db.add(event_tracking_status)
+db.commit()
+
+print "Event Tracking Status Types loaded"
+
 # # Insert Test Event
-event_complete = Event(User.query.filter_by(username=ADMIN_USERNAME).first().id, "seizure", 3, 2)
+event_complete = Event(User.query.filter_by(username=ADMIN_USERNAME).first().id, 'seizure', 3, 2, 'COMPLETE')
 event_incomplete = Event(User.query.filter_by(username=ADMIN_USERNAME).first().id)
 db.add(event_complete)
 db.add(event_incomplete)
--- diary_app/events/models.py
+++ diary_app/events/models.py
@@ -1,8 +1,10 @@
 from diary_app.utils import id_generator
 from diary_app.database import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql import func
+
+## Sqlite doesn't enfore ForeignKey constraints by default
 
 class Event(Base):
     __tablename__ = 'events'
@@ -14,13 +16,31 @@ class Event(Base):
     event_time = Column(DateTime, default=func.now())
     record_created_at = Column(DateTime, default=func.now())
     last_updated = Column(DateTime, onupdate=func.now())
+    event_tracking_status_name = Column(String(32), 
+        ForeignKey('event_tracking_status_types.name'), nullable=False)
+    event_tracking_status = relationship('EventTrackingStatus', 
+        backref=backref('events', lazy='dynamic'))
     user = relationship('User', backref=backref('jobs', lazy='dynamic'))
 
-    def __init__(self, user_id, event_type=None, event_severity=None, event_duration=None):
+    def __init__(self, user_id, event_type=None, event_severity=None, 
+            event_duration=None, tracking_status_name='CREATED'):
         self.user_id = user_id
         self.event_type = event_type
         self.event_severity = event_severity
         self.event_duration = event_duration
+        self.event_tracking_status_name = tracking_status_name
 
     def __repr__(self):
         return '<Event  %r> for User: %s', [self.id, self.user.username]
+
+
+class EventTrackingStatus(Base):
+    __tablename__ = 'event_tracking_status_types'
+    name = Column(String(32), primary_key=True)
+    description = Column(String(128))   
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<EventTrackingStatus %r>' % self.name
